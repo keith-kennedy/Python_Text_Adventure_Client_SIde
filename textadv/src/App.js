af555affa9544449c9c1ff134989368fcf94c604
keith Kennedy <keith.kennedy@valpo.edu>
2019-02-16 14:07:53 -0600

    have to change another thing inorder for this to work

--- textadv/src/App.js
+++ textadv/src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
       <AppBackground>
       <div>
       <div>
-      <Route path="/registration" render={props => <Home/>} />
+      <Home/> 
       </div>
       <Route path="/registration" render={props => <Registration {...props} login = {this.handleLogin}/>} />
       <Route path="/login" render={props => <Login {...props} login = {this.handleLogin}/>} />
